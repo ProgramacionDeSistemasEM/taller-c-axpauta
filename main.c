b43129a4524f153d0ed77fe11c9ab0c0610092f3
axpauta <alexanderxavier.pauta@gmail.com>
2021-10-19 15:13:29 -0500

    Taller terminado

--- main.c
+++ main.c
@@ -6,7 +6,11 @@ int main(int argc, char **argv){
 	
 	int elementos = 0;
 	char c = 0;
-	
+	int peso;
+	float altura;
+	float imc;
+	float sum=0.0f;
+	float max_imc=0.0f;
 	
 	//Aqui manejamos las opciones.
 	//El argumento -p tiene un argumento, que es 
@@ -15,20 +19,45 @@ int main(int argc, char **argv){
 		switch(c){
 			case 'p':
 				elementos = atoi(optarg);
+				if (elementos<=0){
+					printf("Numero de personas invalido\n");
+					exit(1);
+				}
+
+				for (int i=0; i<elementos; i++){
+					printf("Peso: ");
+                               	        scanf("%d",&peso);
+					if (peso<0){
+					    printf("Numero invalido\n");
+					    exit(1);
+					}
+					printf("Altura: ");
+					scanf("%f",&altura);
+					if (altura<0){
+						printf("Numero invalido\n");
+						exit(1);
+					}
+					imc= peso/(altura*altura);
+					sum= sum+imc;
+					if (imc>max_imc){
+						max_imc=imc;
+				       	}	
+				}
+
 				break;
 			default:
 				printf("Argumento invalido\n");
 				exit(1);
 		}
 	}
-
+	/*
 	//Los resultados de sus calculos van en estas variables.
 	//Puede declarar más variables si lo necesita.
 	float sum = 0.0f;
 	float max_imc = 0.0f;
-	//Comentario de prueba 
-	
-	
+	*/
+
+	sum= sum/elementos;
 	
 	//No modifique estas lineas
 	//Guarde los resultados en las 
